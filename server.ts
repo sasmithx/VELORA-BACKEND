77af85417bb333b4d5379db470c76e17386f0c67
Sasmith Manawadu <sasmithgit@gmail.com>
2025-07-16 09:14:08 +0530

    server.ts completed

--- server.ts
+++ server.ts
@@ -1,5 +1,22 @@
 import express from "express";
+import userRoutes from "./routes/user.routes";
+import cors from "cors";
+import authenticateToken from "./middlewares/auth.middleware";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
+app.use('/api/v1/auth', userRoutes);
+app.use(authenticateToken);
+
+app.listen(5000, (err) => {
+    console.log('Server is running on port 5000');
+});
